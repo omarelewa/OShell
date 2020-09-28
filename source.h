14c6d2828ceb13272afb8f158ad1e434bc686d28
omarelewa <68481601+omarelewa@users.noreply.github.com>
2020-09-28 21:36:00 +0200

    Add files via upload

---
+++ source.h
@@ -0,0 +1,19 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#define EOF (-1)
+#define ERRCHAR ( 0)
+#define INIT_SRC_POS (-2)
+
+struct source_s{
+    char *buffer;   // input text
+    long bufsize;   // size of input text
+    long  curpos;   // absolute char position in source
+};
+
+char next_char (struct source_s *src);
+void unget_char (struct source_s *src);
+char peek_char (struct source_s *src);
+void skip_white_spaces (struct source_s *src);
+
+#endif
