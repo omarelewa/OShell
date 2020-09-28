890b9276fd0ba3b324758a278b3acd58e06d9d9e
omarelewa <68481601+omarelewa@users.noreply.github.com>
2020-09-28 20:00:56 +0200

    Add files via upload

--- prompt.c
+++ prompt.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include "shell.h"
 
 void print_prompt1(void){
@@ -7,4 +10,42 @@ void print_prompt1(void){
 
 void print_prompt2(void){
     fprintf (stderr,"> ");
+}
+
+char *read_cmd(void){
+    char buf [1024];
+    char *ptr = NULL;
+    char ptrlen = 0;
+    
+    while (fgets(buf,1024,stdin)){
+        int buflen = strlen (buf);
+        if (!ptr) {
+            ptr = malloc (buflen+1);
+        }
+        else {
+            char *ptr2 = realloc (ptr, ptrlen+buflen+1);
+            if (ptr2){
+                ptr = ptr2;
+            }
+            else {
+                free (ptr);
+                ptr = NULL;
+            }
+        }
+        if (!ptr){
+            fprintf(stderr, "error: failed to allocate buffer: %s \n", strerror(errno));
+            return NULL;
+        }
+        strcpy(ptr+ptrlen, buf);
+        if (buf[buflen-1] =='\n'){
+            if (buflen == 1 || buf[buflen-2] != '\\'){
+                return ptr;
+            }
+            ptr[ptrlen+buflen-2] = '\0';
+            buflen -= 2;
+            print_prompt2();
+        }
+        ptrlen += buflen;
+    }
+    return ptr;
 }
